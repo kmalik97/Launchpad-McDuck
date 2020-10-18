fbc7d38e93d595991dcbd51ecf08e84ededd6fff
duckiebot <donelp@rpi.edu>
2020-10-18 12:08:54 -0400

    added form of keyboard teleop, added Honglu's Adafruit code for motor hats

--- our_ws/src/launchpad/scripts/motion_interface.py
+++ our_ws/src/launchpad/scripts/motion_interface.py
@@ -1,30 +1,110 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import rospy
+from math import floor
+from Adafruit_MotorHAT import Adafruit_MotorHAT
 from launchpad.srv import motionLogic
 
-# get motion logic and print it
-def print_logic(event):
-    # need to wait until service is available
-    rospy.wait_for_service('motionLogic')
+# define constants
+LEFT_MIN = 0
+LEFT_MAX = 255
+LEFT_RES = LEFT_MAX - LEFT_MIN
+RIGHT_MIN = 0
+RIGHT_MAX = 255
+RIGHT_RES = RIGHT_MAX - RIGHT_MIN
 
-    try:
-        # service callback function handle
-        handle_motion_logic = rospy.ServiceProxy('motionLogic', motionLogic)
+class Motion_Interface:
+    def __init__(self):
+        self.motorhat = Adafruit_MotorHAT(addr=0x60)
+        self.motor_left = self.motorhat.getMotor(1)
+        self.motor_right = self.motorhat.getMotor(2)
+        self.pwm_left = 0
+        self.pwm_right = 0
+        self.linear_vel = 0.0
+        self.angular_vel = 0.0
+        self.timer = rospy.Timer(rospy.Duration(0.1), self.set_motors)
 
-        # service response
-        my_logic = handle_motion_logic()
-        print("motion = [%.2f, %.2f]"%(my_logic.linear_vel, my_logic.angular_vel))
+    # set motors
+    def set_motors(self, event):
+        # get linear and angular velocity, both of which are in range [-1 1]
+        self.get_velocity()
 
-    except rospy.ServiceException as e:
-        print("motionLogic service call failed: %s"%e)
+        # get motor PWM
+        self.get_pwm()
+        
+        print("vel = [%.2f, %.2f], pwm = [%d, %d]"%(self.linear_vel, self.angular_vel, self.pwm_left, self.pwm_right))
+
+        # set motor speed
+        self.motor_left.setSpeed(abs(self.pwm_left))
+        self.motor_right.setSpeed(abs(self.pwm_right))
+
+        # set motor direction
+        if self.pwm_left < 0:
+            self.motor_left.run(Adafruit_MotorHAT.BACKWARD)
+        elif self.pwm_left > 0:
+            self.motor_left.run(Adafruit_MotorHAT.FORWARD)
+        else:
+            self.motor_left.run(Adafruit_MotorHAT.RELEASE)
+
+        if self.pwm_right < 0:
+            self.motor_right.run(Adafruit_MotorHAT.BACKWARD)
+        elif self.pwm_right > 0:
+            self.motor_right.run(Adafruit_MotorHAT.FORWARD)
+        else:
+            self.motor_right.run(Adafruit_MotorHAT.RELEASE)
+
+    # determine motor PWM
+    def get_pwm(self):    
+        # fractional weighting of linear velocity to angular velocity
+        gain = 0.75
+
+        # PWM due to linear velocity
+        pwm_linear_left = floor(self.linear_vel * LEFT_RES + LEFT_MIN)
+        pwm_linear_right = floor(self.linear_vel * RIGHT_RES + RIGHT_MIN)
+
+        # PWM due to angular velocity
+        pwm_angular_left = floor(self.angular_vel * LEFT_RES + LEFT_MIN)
+        pwm_angular_right = floor(self.angular_vel * RIGHT_RES + RIGHT_MIN)
+
+        # need to negate angular velocity if driving backwards
+        if self.linear_vel < 0:
+            pwm_angular_left *= -1
+            pwm_angular_right *= -1
+
+        # total PWM
+        self.pwm_left = -int(gain * pwm_linear_left - (1 - gain) * pwm_angular_left)
+        self.pwm_right = int(gain * pwm_linear_right + (1 - gain) * pwm_angular_right)
+
+    # get linear and angular velocity
+    def get_velocity(self):
+        # need to wait until service is available
+        rospy.wait_for_service('motionLogic')
+
+        try:
+            # service callback function handle
+            handle_motion_logic = rospy.ServiceProxy('motionLogic', motionLogic)
+
+            # service response
+            velocities = handle_motion_logic()
+            self.linear_vel = velocities.linear_vel
+            self.angular_vel = velocities.angular_vel
+
+        except rospy.ServiceException as e:
+            print("motionLogic service call failed: %s"%e)
+
+    # shutdown
+    def on_shutdown(self):
+        self.motor_left.run(Adafruit_MotorHAT.RELEASE)
+        self.motor_right.run(Adafruit_MotorHAT.RELEASE)
+        del self.motorhat
+        print("motion_interface node shut down")
 
 # setup motion logic client
 def client_motion_logic():
     print("initializing motion_interface node")
     rospy.init_node('motion_interface')
-    # print logic every tenth of a second
-    myTimer = rospy.Timer(rospy.Duration(0.1), print_logic)
+    motion_interface = Motion_Interface()
+    rospy.on_shutdown(motion_interface.on_shutdown)
     rospy.spin()
 
 if __name__ == "__main__":
--- our_ws/src/launchpad/scripts/motion_logic.py
+++ our_ws/src/launchpad/scripts/motion_logic.py
@@ -1,40 +1,85 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import rospy
+import sys
+import tty
+import termios
 from launchpad.srv import motionLogic, motionLogicResponse
 
-# determine linear and angular velocity
-def handle_motion_logic(req):
-    # default is no motion
-    linear_vel = 0.0
-    angular_vel = 0.0
-    
-    # get user input
-    inp = raw_input('enter command>> ')
+# read char from terminal
+# from https://github.com/magmax/python-readchar
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setcbreak(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
-    # drive forward
-    if inp == 'w':
-        linear_vel = 0.5
 
-    # drive backward
-    if inp == 's':
-        linear_vel = -0.5
+class Motion_Logic:
+    def __init__(self):
+        self.service = rospy.Service('motionLogic', motionLogic, self.handle_motion_logic)
 
-    # rotate counter-clockwise
-    if inp == 'a':
-        angular_vel = 1.0
+    # determine linear and angular velocity
+    def handle_motion_logic(self, req):
+        # default is no motion
+        linear_vel = 0.0
+        angular_vel = 0.0
+        
+        # get user input
+        inp = readchar()
 
-    # rotate clockwise
-    if inp == 'd':
-        angular_vel = -1.0
+        # drive forward
+        if inp == 'w':
+            linear_vel = 1.0
+        
+        # drive backward
+        if inp == 's':
+            linear_vel = -1.0
+        
+        # rotate counter-clockwise
+        if inp == 'a':
+            angular_vel = 1.0
 
-    return motionLogicResponse(linear_vel, angular_vel)
+        # rotate clockwise
+        if inp == 'd':
+            angular_vel = -1.0
+
+        # forward and right
+        if inp == 'e':
+            linear_vel = 1.0
+            angular_vel = -1.0
+
+        # forward and left
+        if inp == 'q':
+            linear_vel = 1.0
+            angular_vel = 1.0
+
+        # backward and right
+        if inp == 'v':
+            linear_vel = -1.0
+            angular_vel = -1.0
+
+        # backward and left
+        if inp == 'z':
+            linear_vel = -1.0
+            angular_vel = 1.0
+        
+        return motionLogicResponse(linear_vel, angular_vel)
+
+    # shutdown
+    def on_shutdown(self):
+        print("motion_logic node shut down")
 
 # setup motion logic server
 def server_motion_logic():
     print('initializing motion_logic node')
     rospy.init_node('motion_logic')
-    my_service = rospy.Service('motionLogic', motionLogic, handle_motion_logic)
+    motion_logic = Motion_Logic()    
+    rospy.on_shutdown(motion_logic.on_shutdown)
     rospy.spin()
 
 if __name__ == '__main__':
