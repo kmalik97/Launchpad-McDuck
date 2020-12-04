b9a8397d1f5d3dfbd16fe0d6f31ebf04f38ed82c
Lucas Vanslette <vansll@rpi.edu>
2020-12-04 15:50:41 -0800

    added changes to image_processing

--- our_ws/src/launchpad/scripts/image_processing.py
+++ our_ws/src/launchpad/scripts/image_processing.py
@@ -43,9 +43,9 @@ class Image_Processing:
         DIM=(320, 240)
         K=np.array([[157.48126107325643, 0.0, 155.54100584796126], [0.0, 157.4460985081828, 124.1673880384651], [0.0, 0.0, 1.0]])
         D=np.array([[-0.016100807266528787], [-0.0660261841490366], [0.12071322102537432], [-0.07516938046185537]])
-        h,w = imgage.shape[:2]
+        h,w = image.shape[:2]
         map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv2.CV_16SC2)
-        image = cv2.remap(imgage, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+        image = cv2.remap(image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
         
         # Image is now the undistorted image
         
@@ -77,10 +77,10 @@ class Image_Processing:
         # Red Mask 
         mask_red = cv2.inRange(hls, lower_red, upper_red)
         mask_red = cv2.bitwise_and(image,image,mask=mask_red)
-        mask = cv2.dilate(mask, None, iterations=1)
+        mask_red = cv2.dilate(mask_red, None, iterations=1)
         
         # Finding Red Object
-        cnts = cv2.findContours(mask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cv2.findContours(mask_red.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if len(cnts) == 2 else cnts[1]
         
         red_obj_detect = False
@@ -88,11 +88,11 @@ class Image_Processing:
         
         # If cnts is empty
         if not cnts:
-            #print("No Red Found")
+            red_obj_detect = False
         else:
             c = max(cnts, key=cv2.contourArea)
             if cv2.contourArea(c) < threshold:
-                #print("Area under threshold")
+                print("Area under threshold")
             else:
                 print('Red Object Detected Above Threshold')
                 red_obj_detect = True
