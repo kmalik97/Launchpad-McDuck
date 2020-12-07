49a38ba891ab4cbf05a7724a5c31145b88ee0aa5
pat <pat@pat.home>
2020-12-07 18:43:20 -0500

    image undistortion verification in camera_subscriber

--- our_ws/src/launchpad/scripts/calibration/camera_subscriber.py
+++ our_ws/src/launchpad/scripts/calibration/camera_subscriber.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 import cv2
+import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
@@ -17,7 +18,18 @@ def process_image(req):
     bridge = CvBridge()
     try:
         image = bridge.imgmsg_to_cv2(req, "bgr8")
+        
+        # camera parameters
+        DIM = (320,240)
+        K = np.array([[160.023219, 0, 162.971810], [0, 160.263543, 123.423868], [0, 0, 1]])
+        D = np.array([[-0.266205], [0.045222], [-0.001402], [-0.000906]])
+        
+        # undistort
+        K_new, roi = cv2.getOptimalNewCameraMatrix(K, D, DIM, 1, DIM)
+        und = cv2.undistort(image, K, D, None, K_new)
+
         cv2.imshow('image', image)
+        cv2.imshow('und', und)
         cv2.waitKey(100)
     except CvBridgeError:
         pass
